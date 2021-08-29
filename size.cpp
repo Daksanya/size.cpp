6defb38f9b5bebeb887062486f17808a6f11b778
Daksanya Nandhakumar <75685853+Daksanya@users.noreply.github.com>
2021-08-29 10:47:30 +0530

    Create size.cpp

---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n Int size is:"<< sizeof(int);
+	cout<<"\n Float size is:"<< sizeof(float);
+	cout<<"\n Double size is:"<< sizeof(double);
+	cout<<"\n Char size is:"<< sizeof(char);
+	return 0;
+}
